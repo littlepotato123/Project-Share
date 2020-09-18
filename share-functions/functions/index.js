72dab58086c10cb017a7045c5fc2581f732100e6
Om Shivam Panda <sopanda0910@gmail.com>
2020-09-18 18:37:51 -0400

    hi

--- share-functions/functions/index.js
+++ share-functions/functions/index.js
@@ -1,1 +1,8 @@
 const functions = require('firebase-functions');
+const app = require('express')();
+
+app.get('/sample', (req, res) => {
+  res.send('This works')
+})
+
+exports.api = functions.https.onRequest(app);
