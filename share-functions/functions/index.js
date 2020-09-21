ec1adc4f9564db1dc0894d96c0f3c0e5628622cf
Om Shivam Panda <sopanda0910@gmail.com>
2020-09-21 08:05:14 -0400

    Setting Backend Up

--- share-functions/functions/index.js
+++ share-functions/functions/index.js
@@ -1,11 +1,33 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const app = require('express')();
+admin.initializeApp();
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+const db = admin.firestore()
 
 // Creating Posts
+app.post('/createPost', (req, res) => {
+  const newPost = {
+    body: req.body.body,
+    author: req.body.author,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+    title: req.body.title,
+    category: req.body.category,
+    likes: 0,
+    comments: 0
+  }
+
+  db
+    .collection('posts')
+    .add(newPost)
+    .then(doc => {
+      res.json({ message: `${doc.id} has been created successfully` })
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'something went wrong' })
+      console.error(err);
+    })
+})
 
 // Sign Up
 
@@ -13,12 +35,49 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 
 // Liking Posts
 
+// Commenting Posts
+
 // Following User
 
 // Getting Random Posts
+app.get('/getAll', (req, res) => {
+  db
+    .collection('posts')
+    .get()
+    .then(data => {
+      let posts = [];
+      data.forEach(doc => {
+        posts.push(doc.data());
+      });
+      return res.json(posts);
+    })
+    .catch(err => {
+      console.error(err);
+    })
+})
 
 // Getting Popular Posts
 
 // Getting Leaderboard Users
 
 // Getting Top 20 Users
+
+// Creating Category
+app.post('/newCategory', (req, res) => {
+  const newCategory = {
+    title: req.body.title,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+  };
+
+  db
+    .collection('categories')
+    .add(newCategory)
+    .then(doc => {
+      res.json({ message: `${doc.id} created successfully` })
+    })
+    .catch(err => {
+      console.error(err);
+    })
+})
+
+exports.api = functions.https.onRequest(app);
