2ee722d8ce3e4f3c61062a0eb5959884218ca1f2
Om Shivam Panda <sopanda0910@gmail.com>
2020-09-23 15:51:24 -0400

    Added Authentication

--- share-functions/functions/index.js
+++ share-functions/functions/index.js
@@ -3,6 +3,21 @@ const admin = require('firebase-admin');
 const app = require('express')();
 admin.initializeApp();
 
+const firebase = require('firebase');
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDNQRLwHXC_zYcknNdf1rplcYpBP2qIKxA",
+  authDomain: "project-share-8df06.firebaseapp.com",
+  databaseURL: "https://project-share-8df06.firebaseio.com",
+  projectId: "project-share-8df06",
+  storageBucket: "project-share-8df06.appspot.com",
+  messagingSenderId: "242999336210",
+  appId: "1:242999336210:web:43105302d4336a9113a7cb",
+  measurementId: "G-FXCLV7D7WE"
+};
+
+firebase.initializeApp(firebaseConfig);
+
 const db = admin.firestore()
 
 // Creating Posts
@@ -30,8 +45,112 @@ app.post('/createPost', (req, res) => {
 })
 
 // Sign Up
+const isEmpty = (string) => {
+  if(string.trim() === '') return true;
+  else return false;
+}
+
+const isEmail = (email) => {
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if(email.match(regEx)) return true;
+  else return false;
+}
+
+app.post('/signup', (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    userHandle: req.body.userHandle
+  };
+
+  let errors = {};
+  if(isEmpty(newUser.email)) {
+    errors.email = 'Must not be empty';
+  } else {
+    errors.email = 'Must be a valid email address';
+  }
+
+  if(isEmpty(newUser.password)) errors.password = 'Must not be empty';
+  if(newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'Password must match';
+
+  if(isEmpty(newUser.userHandle)) errors.userHandle = 'Must not be empty';
+
+  if(Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+  // Validate Data
+  let idToken;
+  let userId;
+  db
+    .doc(`/users/${newUser.userHandle}`)
+    .get()
+    .then(doc => {
+      if(doc.exists) {
+        return res.status(400).json({ handle: 'This handle already exists' })
+      } else {
+        return firebase
+          .auth()
+          .createUserWithEmailAndPassword(newUser.email, newUser.password);
+      }
+    })
+    .then(data => {
+      userId = data.user.uid;
+      return data.user.getIdToken()
+    })
+    .then(token => {
+      idToken = token;
+      const userCred = {
+        handle: newUser.userHandle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId: userId
+      };
+      return db
+        .doc(`/users/${newUser.userHandle}`)
+        .set(userCred)
+    })
+    .then(() => {
+      return res.status(201).json({ idToken })
+    })
+    .catch(err => {
+      if(err.code === 'auth/email-already-in-use') {
+        return res.status(400).json({ email: 'Email is already in use' })
+      } else {
+        res.status(500).json({ error: err.code })
+      }
+    })
+})
 
 // Login
+app.post('/login', (req, res) => {
+  const user = {
+    email: req.body.email,
+    password: req.body.password
+  }
+
+  let errors = {};
+
+  if(isEmpty(user.email)) errors.email = 'Must not be empty';
+  if(isEmpty(user.password)) errors.password = 'Must not be empty';
+
+  if(Object.keys(errors).length > 0) return res.json(errors);
+
+  firebase.auth()
+    .signInWithEmailAndPassword(user.email, user.password)
+    .then(data => {
+      return data.user.getIdToken();
+    })
+    .then(token => {
+      return res.json({token});
+    })
+    .catch(err => {
+      if(err.code === 'auth/wrong-password') {
+        return res.status(403).json({ general: 'Wrong Credentials, please try again' })
+      } else {
+        res.status(500).json({ error: err.code });
+      }
+    })
+})
 
 // Liking Posts
 app.post('/likePost', (req, res) => {
