14b91d29ff24af0971d60e13f103a5d32f3c49e0
Om Panda <sopanda0910@gmail.com>
2020-10-13 06:03:19 -0400

    Basics of User

--- project-share/src/Pages/User Page/User.js
+++ project-share/src/Pages/User Page/User.js
@@ -1,41 +1,50 @@
 import React, { useEffect, useState } from 'react'
-import { useParams, useHistory } from 'react-router-dom';
+import {
+    useHistory,
+    useParams
+} from 'react-router-dom'
 
-const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-const url = 'https://us-central1-project-share-8df06.cloudfunctions.net/api/';
+const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+const url = "https://us-central1-project-share-8df06.cloudfunctions.net/api/";
 
 const User = () => {
+    const [user, setUser] = useState({});
+
     const { userHandle } = useParams();
-    const [user, setUser] = useState(null);
-    const [count, setCount] = useState(0);
+    
     const history = useHistory();
 
     useEffect(() => {
-        if(count < 2) {
-            fetch(proxyUrl + url + 'getUser', {
-                method: 'POST',
-                body: JSON.stringify({
-                    "userHandle": userHandle
-                }),
-                headers: {
-                    'Content-Type': 'application/json',
-                }
+        fetch(proxyUrl + url + 'getUser', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                "User-Agent": "PostmanRuntime/7.26.5",
+                "Accept": "*/*",
+                "Accept-Encoding": "gzip, deflate, br",
+                "Connection": "keep-alive"
+            },
+            body: JSON.stringify({
+                userHandle: userHandle
             })
-            .then(res => res.json())
-            .then(data => {
-                if(data.user == undefined) {
-                    history.push('/wronguser');
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(typeof data == 'object') {
+                if(Object.keys(data).length === 0) {
+                    console.log("empty");
                 } else {
+                    console.log("Not Empty");
+                    console.log(data.user)
                     setUser(data.user);
                 }
-            })
-            setCount(count + 1);
-        }
+            }
+        })
     }, [])
 
     return (
         <div>
-            Hello
+            Hello, { user.handle }
         </div>
     )
 }
