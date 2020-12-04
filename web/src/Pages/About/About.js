ad4119cac718c3f459dd30fee9fddce84c5c91ce
Arora <arora.akshat2007@gmail.com>
2020-12-04 18:57:46 -0500

    sample

--- web/src/Pages/About/About.js
+++ web/src/Pages/About/About.js
@@ -7,11 +7,15 @@ const About = () => {
         What is <a href="/home"> Project Sh@re </a>
       </h1>
       <p>
-        <a href="/home"> Project Sh@re </a> is a platform created to support users who wish to aprise{" "}
+        <a href="/home"> Project Sh@re </a> is a platform created to support users who wish to aprise others about{" "}
         <br />
-        others about difficulties and problems being tackled across the globe.{" "}
+        difficulties and problems being tackled across the globe. As a community, we discuss {" "}
         <br />
-        Project Sh@re is welcoming to all users to provoke change. 
+        to improve oursleves and determine solutions to mistakes and problems we cause in our daily
+         <br />
+         lives. As people we need to better oursleves and <a href="/home"> Project Sh@re </a> gives us an opporunity.
+        <br />
+        <a href="/home"> Project Sh@re </a> is welcoming to all users to provoke change.
       </p>
     </div>
   );
