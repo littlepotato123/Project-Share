7dedfc9c7f3c962130e5b58c3a3bf188e60abb77
Arora <arora.akshat2007@gmail.com>
2021-01-15 18:45:17 -0500

    about page styling

--- web/src/Pages/About/About.js
+++ web/src/Pages/About/About.js
@@ -34,7 +34,7 @@ const About = () => {
               our daily lives. As people, we need to better ourselves and <a href="/home"> Project Sh@re</a> gives us an
               opportunity. {href} is welcoming to all users to provoke change. Users may interact
               with one another to share their ideas. Here, we build thoughts,
-              wonder, and supporters to a common cause. {/* Style seperately - stand out*/}  Welcome to {href}!
+              wonder, and supporters to a common cause. Welcome to {href}!
       </p>
       <h2 className="whyTitle">Why {href}?</h2>
       <p className="why_paragraph">
@@ -42,7 +42,7 @@ const About = () => {
           different? </i> {href} is specifically created to help discuss world-wide issues. A platform
           only used to inform, learn, and promote. Not all communities thrive as ours do; it's important
           for the world to understand different and newer points of view. This platform is not used for any
-        personal gain.{/* Style seperately - stand out*/} At {href}, we take part in making the world a better place!
+        personal gain. At {href}, we take part in making the world a better place!
         </p>
 
       <h2 className="policyTitle">Our Policy</h2>
@@ -55,52 +55,6 @@ const About = () => {
           period of time</li>
         </ol>
       </p>
-
-      {/* <h2>Pages</h2>
-      <p>
-        <strong><a href="/home"> Home: </a></strong> The <a href="/home"> home </a> page contains a navigation bar that allows users to access other pages,
-        <br />
-        log in/out, sign up/in, review their accounts, and post. It also contains randomly selected
-        <br />
-        posts which are available to everyone.
-        <br />
-        <br />
-        <strong><a href="/trending">Trending: </a></strong> The <a href="/trending"> trending </a> page contains the top 10 most liked posts by the {href}
-        <br />
-        community. It's being updated live as users continue to like and comment on posts.
-        <br />
-        <br />
-        <strong><a href="/leaderboard">Leaderboard: </a></strong> The <a href="/leaderboard">leaderboard</a> page contains the top 5 users in the {href}
-        <br /> community. It's being updated live as users continue to grow and support others.
-        <br />
-        <br />
-        <strong><a href="/categories">Categories: </a></strong> The <a href="/categories">categories</a> page contains all the different categories/topics supported
-        <br />
-        supported {href}. There is a suggestions' box in the <a href="/about">about </a> page which allows
-        <br />
-        users to give their suggestions about categories.
-        <br />
-        <br />
-        <strong><a href="/about">About:</a></strong> The about page describes {href} as a platform and parts of it which users can access. There is a suggestions/concerns
-        <br />
-        box where users can help us improve the platform.
-        <br />
-        <strong><a href="/auth">Authentication:</a></strong> In our <a href="/auth">authenticaiton</a> page, we allow users to log in/out,
-        <br />
-        sign-up, and create posts.
-        <br />
-        <br />
-        <strong><a href="/user/:userHandle">User Page:</a></strong> The user page contains information about {href}'s users, however,
-        <br />
-        only what is approved by the user will be displayed. Information like posts, post count, supporters, supporting, bio, awards, etc.
-        <br />
-        <br />
-        <strong><a href="/newPost">New Post:</a></strong> Creating a post, is its own page; it consists of a title, topic, and a body. When the
-        <br />
-        post is displayed, the audience can identify the user, comments, likes, publish date, title, and category.
-        <br />
-        <br />
-      </p> */}
 
       <h2 className="guestVSuserTitle">Guest VS User</h2>
       <p className="gVSu_paragraph">As members of {href}, users have access to all the features on the platform. However, as guests, they have limited access to the website. Guests are prevented to taking part in the following:
